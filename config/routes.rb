df60a6f62103e4f7adecde940fa406d26210e80a
DankoMaksym <dankomaksym@gmail.com>
2019-11-06 17:40:35 +0200

    show comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   #get '/contacts' => 'contacts#new'
 
-  get 'terms' => 'pages#terms'
-  get 'about' => 'pages#about'
+  get '/terms' => 'pages#terms'
+  get '/about' => 'pages#about'
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
